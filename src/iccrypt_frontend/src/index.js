b6f1664121b34047a9b86c609b6a12c689e08c6d
Tobias <tobias.ambuehl@gmail.com>
2022-12-19 06:17:52 +0100

    Initial commit.

---
+++ src/iccrypt_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { iccrypt_backend } from "../../declarations/iccrypt_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await iccrypt_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
