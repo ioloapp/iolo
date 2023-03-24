66f9c9a44cc8fd1a03bcd7c2ce66a6ee2a331cdc
Tobias <tobias.ambuehl@gmail.com>
2023-03-24 12:05:12 +0100

    fist happy flow works

--- tests/iccrypt-tests/src/key_derivation.rs
+++ tests/iccrypt-tests/src/key_derivation.rs
@@ -2,64 +2,85 @@ use anyhow::Result;
 use candid::{Encode, Principal};
 use ic_agent::{identity::BasicIdentity, Agent, Identity};
 use openssl::{
-    encrypt::Encrypter,
-    pkey::Public,
+    pkey::{Private, Public},
     rsa::{Padding, Rsa},
 };
 
 use crate::common::{create_identity, get_dfx_agent_with_identity, get_iccrypt_backend_canister};
 pub async fn test_key_derivation() -> Result<()> {
+    // test the phase I self encryption
+    self_encryption().await?;
+    Ok(())
+}
+
+async fn self_encryption() -> Result<()> {
     let canister = get_iccrypt_backend_canister();
 
     // create identities (keypairs) and the corresponding senders (principals)
-    let identity: BasicIdentity = create_identity();
-    let principal: Principal = identity.sender().unwrap();
-    let agent: Agent = get_dfx_agent_with_identity(identity).await?;
+    let alice = "Alice";
+    let eve = "Eve";
+    let identity_alice: BasicIdentity = create_identity();
+    let principal_alice: Principal = identity_alice.sender().unwrap();
+    let agent_alice: Agent = get_dfx_agent_with_identity(identity_alice).await?;
 
-    // get encryption key
-    let res: Vec<u8> = agent
+    let identity_eve: BasicIdentity = create_identity();
+    let principal_eve: Principal = identity_eve.sender().unwrap();
+    let agent_eve: Agent = get_dfx_agent_with_identity(identity_eve).await?;
+
+    // get encryption key for alice
+    let res: Vec<u8> = agent_alice
         .update(&canister, "get_encryption_key_for")
         // .with_arg(&Encode!(&principal.to_string())?)
-        .with_arg(&Encode!(&"Bob")?)
+        .with_arg(&Encode!(&alice)?)
         .call_and_wait()
         .await
         .unwrap();
 
     let mut res_deserialized = candid::de::IDLDeserialize::new(&res)?;
 
-    let pk: Rsa<Public>;
+    let enc_key_alice: Rsa<Public>;
     if let Some(enc_key) = res_deserialized.get_value::<Option<Vec<u8>>>().unwrap() {
-        let encryption_key = enc_key;
-        pk = openssl::rsa::Rsa::public_key_from_pem(&encryption_key).unwrap();
+        enc_key_alice = openssl::rsa::Rsa::public_key_from_pem(&enc_key).unwrap();
     } else {
         return Err(anyhow::format_err!("Failed getting encryption key"));
     }
 
     // let's encrypt a message
-    let data = b"hello, bob, i hope you are doing well!";
+    let secret_alice = "This is my super secret";
     //let buffer_len = encrypter.encrypt_len(data).unwrap();
-    let mut encrypted_message = vec![0; 1024];
-    let enc_res = pk.public_encrypt(data, &mut encrypted_message, Padding::PKCS1);
+    let mut encrypted_secret: Vec<u8> = vec![0; enc_key_alice.size() as usize];
+    enc_key_alice
+        .public_encrypt(
+            secret_alice.as_bytes(),
+            &mut encrypted_secret,
+            Padding::PKCS1,
+        )
+        .unwrap();
 
-    dbg!(enc_res);
-    dbg!(&encrypted_message);
+    dbg!(&encrypted_secret);
 
-    // get decryption key
-    let res: Vec<u8> = agent
+    // get decryption key for alice
+    let res: Vec<u8> = agent_alice
         .update(&canister, "get_decryption_key_from")
-        .with_arg(&Encode!(&principal.to_string())?)
+        .with_arg(&Encode!(&alice)?)
         .call_and_wait()
         .await
         .unwrap();
 
     res_deserialized = candid::de::IDLDeserialize::new(&res)?;
 
-    let decryption_key: Vec<u8>;
+    let dec_key_alice: Rsa<Private>;
     if let Some(dec_key) = res_deserialized.get_value::<Option<Vec<u8>>>().unwrap() {
-        decryption_key = dec_key;
+        dec_key_alice = openssl::rsa::Rsa::private_key_from_pem(&dec_key).unwrap();
     } else {
         return Err(anyhow::format_err!("Failed getting decryption key"));
     }
+    let mut decrypted_secret: Vec<u8> = vec![0; dec_key_alice.size() as usize];
+    let x = dec_key_alice
+        .private_decrypt(&encrypted_secret, &mut decrypted_secret, Padding::PKCS1)
+        .unwrap();
+
+    dbg!(String::from_utf8(decrypted_secret).unwrap());
 
     Ok(())
 }
