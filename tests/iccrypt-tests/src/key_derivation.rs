e289801712c5735fc0287c7a8465039c8b447353
Tobias <tobias.ambuehl@gmail.com>
2023-03-24 14:24:37 +0100

    cleanup

--- tests/iccrypt-tests/src/key_derivation.rs
+++ tests/iccrypt-tests/src/key_derivation.rs
@@ -14,73 +14,98 @@ pub async fn test_key_derivation() -> Result<()> {
 }
 
 async fn self_encryption() -> Result<()> {
-    let canister = get_iccrypt_backend_canister();
-
     // create identities (keypairs) and the corresponding senders (principals)
     let alice = "Alice";
     let eve = "Eve";
     let identity_alice: BasicIdentity = create_identity();
     let principal_alice: Principal = identity_alice.sender().unwrap();
     let agent_alice: Agent = get_dfx_agent_with_identity(identity_alice).await?;
-
     let identity_eve: BasicIdentity = create_identity();
     let principal_eve: Principal = identity_eve.sender().unwrap();
     let agent_eve: Agent = get_dfx_agent_with_identity(identity_eve).await?;
 
+    let secret = "hey, this is my secret. how is life my dear friend?";
+
+    println!("The original secfet: {}", &secret);
+
+    // the happy flow: self encryption for alice
+    let encrypted_secret = encrypt_secret_for(&agent_alice, alice, secret).await?;
+    let decrypted_secret = decrypt_secret_from(&agent_alice, alice, &encrypted_secret).await?;
+    println!(
+        "This is what Alice sees after decryption: {}",
+        &decrypted_secret
+    );
+
+    // eve dropping in
+    let decrypted_secret = decrypt_secret_from(&agent_eve, alice, &encrypted_secret).await?;
+    println!(
+        "This is what Eve sees after decryption: {}",
+        &decrypted_secret
+    );
+
+    Ok(())
+}
+
+async fn encrypt_secret_for(agent: &Agent, recipient: &str, secret: &str) -> Result<Vec<u8>> {
+    let canister = get_iccrypt_backend_canister();
+
     // get encryption key for alice
-    let res: Vec<u8> = agent_alice
+    let res: Vec<u8> = agent
         .update(&canister, "get_encryption_key_for")
         // .with_arg(&Encode!(&principal.to_string())?)
-        .with_arg(&Encode!(&alice)?)
+        .with_arg(&Encode!(&recipient)?)
         .call_and_wait()
         .await
         .unwrap();
 
     let mut res_deserialized = candid::de::IDLDeserialize::new(&res)?;
 
-    let enc_key_alice: Rsa<Public>;
+    let enc_key_recipient: Rsa<Public>;
     if let Some(enc_key) = res_deserialized.get_value::<Option<Vec<u8>>>().unwrap() {
-        enc_key_alice = openssl::rsa::Rsa::public_key_from_pem(&enc_key).unwrap();
+        enc_key_recipient = openssl::rsa::Rsa::public_key_from_pem(&enc_key).unwrap();
     } else {
         return Err(anyhow::format_err!("Failed getting encryption key"));
     }
 
+    // dbg!(&enc_key_recipient.public_key_to_der());
+
     // let's encrypt a message
-    let secret_alice = "This is my super secret";
-    //let buffer_len = encrypter.encrypt_len(data).unwrap();
-    let mut encrypted_secret: Vec<u8> = vec![0; enc_key_alice.size() as usize];
-    enc_key_alice
-        .public_encrypt(
-            secret_alice.as_bytes(),
-            &mut encrypted_secret,
-            Padding::PKCS1,
-        )
+    let mut encrypted_secret: Vec<u8> = vec![0; enc_key_recipient.size() as usize];
+    enc_key_recipient
+        .public_encrypt(secret.as_bytes(), &mut encrypted_secret, Padding::PKCS1)
         .unwrap();
+    Ok(encrypted_secret)
+}
 
-    dbg!(&encrypted_secret);
+async fn decrypt_secret_from(
+    agent: &Agent,
+    sender: &str,
+    encrypted_secret: &[u8],
+) -> Result<String> {
+    let canister = get_iccrypt_backend_canister();
 
-    // get decryption key for alice
-    let res: Vec<u8> = agent_alice
+    // get decryption key
+    let res: Vec<u8> = agent
         .update(&canister, "get_decryption_key_from")
-        .with_arg(&Encode!(&alice)?)
+        .with_arg(&Encode!(&sender)?)
         .call_and_wait()
         .await
         .unwrap();
 
-    res_deserialized = candid::de::IDLDeserialize::new(&res)?;
+    let mut res_deserialized = candid::de::IDLDeserialize::new(&res)?;
 
-    let dec_key_alice: Rsa<Private>;
+    let decryption_key: Rsa<Private>;
     if let Some(dec_key) = res_deserialized.get_value::<Option<Vec<u8>>>().unwrap() {
-        dec_key_alice = openssl::rsa::Rsa::private_key_from_pem(&dec_key).unwrap();
+        decryption_key = openssl::rsa::Rsa::private_key_from_pem(&dec_key).unwrap();
     } else {
         return Err(anyhow::format_err!("Failed getting decryption key"));
     }
-    let mut decrypted_secret: Vec<u8> = vec![0; dec_key_alice.size() as usize];
-    let x = dec_key_alice
+
+    // decrypt the secret
+    let mut decrypted_secret: Vec<u8> = vec![0; decryption_key.size() as usize];
+    let x = decryption_key
         .private_decrypt(&encrypted_secret, &mut decrypted_secret, Padding::PKCS1)
         .unwrap();
 
-    dbg!(String::from_utf8(decrypted_secret).unwrap());
-
-    Ok(())
+    Ok(String::from_utf8(decrypted_secret).unwrap())
 }
